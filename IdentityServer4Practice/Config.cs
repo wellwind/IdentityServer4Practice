f0403f7e4b5cf8f994c6dd79e3d0d539fc1a5ffe
wellwind <wellwind@mail2000.com.tw>
2017-09-04 15:24:48 +0800

    加入允許的clients

--- IdentityServer4Practice/Config.cs
+++ IdentityServer4Practice/Config.cs
@@ -16,5 +16,24 @@ namespace IdentityServer4Practice
                 new ApiResource("api1", "My API")
             };
         }
+
+        // 允許的clients
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
     }
 }
