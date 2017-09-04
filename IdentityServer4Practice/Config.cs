9a8624fcba5e7d6846cccff3c4f4e42a2a39d3a8
wellwind <wellwind@mail2000.com.tw>
2017-09-04 15:23:31 +0800

    加入Config並定義預設scopes

---
+++ IdentityServer4Practice/Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+
+namespace IdentityServer4Practice
+{
+    public class Config
+    {
+        // 定義scope
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+    }
+}
