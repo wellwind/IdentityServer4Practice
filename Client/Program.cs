dd735c3bb3ae224b4614e3fe1c2628f49a098730
wellwind <wellwind@mail2000.com.tw>
2017-09-05 14:08:45 +0800

    從IdentityServer取得access token

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using IdentityModel.Client;
 
 namespace Client
 {
@@ -6,7 +8,31 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync()
+        {
+            var accessToken = await getAccessToken();
+            Console.WriteLine(accessToken);
+        }
+
+        private static async Task<string> getAccessToken()
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return null;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            return tokenResponse.AccessToken;
         }
     }
 }
