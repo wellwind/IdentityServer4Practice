7cb1e436a629bd95ffe1c2ceedbede06b8b79d6f
wellwind <wellwind@mail2000.com.tw>
2017-09-05 15:47:05 +0800

    About頁面設定為須登入授權

--- MvcClient/Controllers/HomeController.cs
+++ MvcClient/Controllers/HomeController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MvcClient.Controllers
 {
+
     public class HomeController : Controller
     {
         public IActionResult Index()
@@ -13,6 +15,7 @@ namespace MvcClient.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
