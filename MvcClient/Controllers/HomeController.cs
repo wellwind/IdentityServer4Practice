50c849a32e1792505fadec3c154f6969a1e9f725
wellwind <wellwind@mail2000.com.tw>
2017-09-05 18:01:13 +0800

    使用取得的access token抓api資料

--- MvcClient/Controllers/HomeController.cs
+++ MvcClient/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json.Linq;
 
 namespace MvcClient.Controllers
 {
@@ -16,9 +20,16 @@ namespace MvcClient.Controllers
         }
 
         [Authorize]
-        public IActionResult About()
+        public async Task<IActionResult> About()
         {
             ViewData["Message"] = "Your application description page.";
+
+            var accessToken = await HttpContext.Authentication.GetTokenAsync("access_token");
+            var client = new HttpClient();
+            client.SetBearerToken(accessToken);
+            var content = await client.GetStringAsync("http://localhost:5001/identity");
+
+            ViewBag.ApiData = JArray.Parse(content).ToString();
 
             return View();
         }
