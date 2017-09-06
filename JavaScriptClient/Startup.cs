df8130ac2c6bd665569fdf51cc41f4dea662b32e
wellwind <wellwind@mail2000.com.tw>
2017-09-06 09:58:34 +0800

    設定StaticFiles

--- JavaScriptClient/Startup.cs
+++ JavaScriptClient/Startup.cs
@@ -21,6 +21,9 @@ namespace JavaScriptClient
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             loggerFactory.AddConsole();
 
             if (env.IsDevelopment())
